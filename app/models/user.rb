4b20970fe1fea38d3c36284d319ba7d6bdd57b8b
Raúl Villares <raul@devengo.com>
2024-03-08 16:28:22 +0100

    Completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
   validates :email, presence: true
   normalizes :email, with: -> (email) { email.strip.downcase }
+
+  generates_token_for :password_reset, expires_in: 15.minutes do
+    password_salt&.last(10)
+  end
 end
